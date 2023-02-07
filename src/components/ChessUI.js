c3ee9df877871d9486f572d160a4814e7ea3a555
Esai Jimenez <ecjimenez26@gmail.com>
2023-02-07 17:39:01 -0600

    Second commit

--- src/components/ChessUI.js
+++ src/components/ChessUI.js
@@ -4,18 +4,13 @@ import { Chess } from 'chess.js';
 
 
 const ChessUI = () => {
-    //const [game, setGame] = useState(new Chess());
-
-
-
-
 
 
 
 
     return (
         <div className="Chess">
-            <h1>ChessUI</h1>
+
             <Chessboard
                 position={"r2q1rk1\/pp2b1p1\/2p3P1\/3n1p2\/3P4\/P1NQ4\/1PKB1PP1\/7R b - - 4 20"} />
         </div>
