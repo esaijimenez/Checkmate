a365f4c355e38d5e2d1d3c4b918818b8f527eb45
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-02-28 14:03:12 -0600

    Button fixes
    All buttons should now only be changed on the main menu ui page.

--- src/components/MainMenuUI.js
+++ src/components/MainMenuUI.js
@@ -24,12 +24,12 @@ export default class MainMenuUI extends React.Component {
                 <br />
                 <div className = "buttons-container">
                 <Link to="/"></Link>
-                <Link to="/gamemode"><button>Play</button></Link>
-                <Link to="/custom-puzzles"><button>Custom Puzzles</button></Link>
-                <Link to="/leaderboard"><button>Leaderboard</button></Link>
-                <Link to="/help-tutorial"><button>Help</button></Link>
-                <Link to="/settings"><button>Settings</button></Link>
-                <Link to="/login"><button>Login</button></Link>
+                <Link to="/gamemode"><button class = "button">Play</button></Link>
+                <Link to="/custom-puzzles"><button class = "button">Custom Puzzles</button></Link>
+                <Link to="/leaderboard"><button class = "button">Leaderboard</button></Link>
+                <Link to="/help-tutorial"><button class = "button">Help</button></Link>
+                <Link to="/settings"><button class = "button">Settings</button></Link>
+                <Link to="/login"><button class = "button">Login</button></Link>
                 </div>
             </div>
             </div>
