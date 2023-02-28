cd4dc05daf53131822e766095ba4a701e2698480
Esai Jimenez <ejimenez4@my.apsu.edu>
2023-02-28 13:59:06 -0600

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles/Navbar.css'
 
 export default class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar">
                 <Link to="/"></Link>
-                <Link to="/gamemode"><button>Play</button></Link>
-                <Link to="/custom-puzzles"><button>Custom Puzzles</button></Link>
-                <Link to="/leaderboard"><button>Leaderboard</button></Link>
-                <Link to="/help-tutorial"><button>Help</button></Link>
-                <Link to="/settings"><button>Settings</button></Link>
-                <Link to="/login"><button>Login</button></Link>
+                <Link to="/gamemode"><button type="button" class="button">Play</button></Link>
+                <Link to="/custom-puzzles"><button type="button" class="button">Custom Puzzles</button></Link>
+                <Link to="/leaderboard"><button type="button" class="button">Leaderboard</button></Link>
+                <Link to="/help-tutorial"><button type="button" class="button">Help</button></Link>
+                <Link to="/settings"><button type="button" class="button">Settings</button></Link>
+                <Link to="/login"><button type="button" class="button">Login</button></Link>
             </div>
         )
     }
