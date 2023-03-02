e4e80ca61670ac550e28c35f6acb5453e7af1c5a
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-03-02 13:16:04 -0600

    Custom Puzzles ui styling
    Styling consistent with classical/bullet game mode ui screen.

--- src/components/CustomPuzzlesUI.js
+++ src/components/CustomPuzzlesUI.js
@@ -1,29 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import '../styles/CustomPuzzlesUI-style.css'
+import "../styles/CustomPuzzlesUI-style.css";
 
 export default class CustomPuzzlesUI extends React.Component {
+  render() {
+    return (
+      <div className="customPuzzle">
+        <Link to="/">
+          <button>Back</button>
+        </Link>
 
-    render() {
-        return (
-            <div className = "customPuzzle">
-                <Link to="/"><button>Back</button></Link>
-
-                <div className = "customPuzzle-container">
-                    <div className = "customPuzzle-item">
-                    <h1 class = "customPuzzle-title">Create</h1>
-                    <p class = "customPuzzle-caption">Find the mates at your own pace.</p>
-                    <Link to="/create"><button class = "customPuzzle-button">Create</button></Link>
-                    </div>
-
-                    <div className = "customPuzzle-item">
-                    <h1 class = "customPuzzle-title">Play</h1>
-                    <p class = "customPuzzle-caption">Find the mates before time runs out.</p>
-                    <Link to="/play"><button class = "customPuzzle-button">Play</button></Link>
-                    </div>
-                </div>
+        <div className="customPuzzle-container">
+          <div className="customPuzzle-item">
+            <h1 class="customPuzzle-title">Create</h1>
+            <p class="customPuzzle-caption">Find the mates at your own pace.</p>
+            <div className="customPuzzle-buttons-container">
+              <Link to="/create">
+                <button class="customPuzzle-button">Create</button>
+              </Link>
             </div>
-        );
-    }
+          </div>
+
+          <div className="customPuzzle-item">
+            <h1 class="customPuzzle-title">Play</h1>
+            <p class="customPuzzle-caption">
+              Find the mates before time runs out.
+            </p>
+            <div className="customPuzzle-buttons-container">
+              <Link to="/play">
+                <button class="customPuzzle-button">Play</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/GameModeUI.js
+++ src/components/GameModeUI.js
@@ -1,33 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import '../styles/GameModeUI-style.css'
+import "../styles/GameModeUI-style.css";
 
 export default class GameModeUI extends React.Component {
+  render() {
+    return (
+      <div className="gameMode">
+        <Link to="/">
+          <button>Back</button>
+        </Link>
 
-    render() {
-        return (
-            <div className = "gameMode">
-                <Link to="/"><button>Back</button></Link>
-
-                <div className = "gameMode-container">
-                    <div className = "classical-item">
-                    <h1 class = "gameMode-title">Classical Mate</h1>
-                    <p class = "gameMode-caption">Find the mates at your own pace.</p>
-                    <div className = "gameMode-buttons-container">
-                    <Link to="/classical"><button class = "gameMode-button">Classical</button></Link>
-                    </div>
-                    </div>
-
-                    <div className = "bullet-item">
-                    <h1 class = "gameMode-title">Bullet Mate</h1>
-                    <p class = "gameMode-caption">Find the mates before time runs out.</p>
-                    <div className = "gameMode-buttons-container">
-                    <Link to="/bullet"><button class = "gameMode-button">Bullet</button></Link>
-                    </div>
-                    </div>
-                </div>
+        <div className="gameMode-container">
+          <div className="classical-item">
+            <h1 class="gameMode-title">Classical Mate</h1>
+            <p class="gameMode-caption">Find the mates at your own pace.</p>
+            <div className="gameMode-buttons-container">
+              <Link to="/classical">
+                <button class="gameMode-button">Classical</button>
+              </Link>
             </div>
-        );
-    }
+          </div>
+
+          <div className="bullet-item">
+            <h1 class="gameMode-title">Bullet Mate</h1>
+            <p class="gameMode-caption">Find the mates before time runs out.</p>
+            <div className="gameMode-buttons-container">
+              <Link to="/bullet">
+                <button class="gameMode-button">Bullet</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
