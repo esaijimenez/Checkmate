3ed64fb6809808a85d14d8dbabdb95b6dfa20166
Esai Jimenez <ejimenez4@my.apsu.edu>
2023-03-02 13:03:44 -0600

    commit

--- src/components/CustomPuzzlesUI.js
+++ src/components/CustomPuzzlesUI.js
@@ -1,24 +1,28 @@
 import React from "react";
-import ChessUI from "./ChessUI";
 import { Link } from "react-router-dom";
 
-export default class CustomPuzzlesUI extends ChessUI {
-    constructor(props) {
-        super(props);
-        this.state = {
+import '../styles/CustomPuzzlesUI-style.css'
 
-        };
-    };
-
-    componentDidMount() {
-
-    };
+export default class CustomPuzzlesUI extends React.Component {
 
     render() {
         return (
-            <div className="customPuzzles">
+            <div className = "customPuzzle">
                 <Link to="/"><button>Back</button></Link>
-                <h1>CustomPuzzlesUI</h1>
+
+                <div className = "customPuzzle-container">
+                    <div className = "customPuzzle-item">
+                    <h1 class = "customPuzzle-title">Classical Mate</h1>
+                    <p class = "customPuzzle-caption">Find the mates at your own pace.</p>
+                    <Link to="/classical"><button class = "customPuzzle-button">Classical</button></Link>
+                    </div>
+
+                    <div className = "customPuzzle-item">
+                    <h1 class = "customPuzzle-title">Bullet Mate</h1>
+                    <p class = "customPuzzle-caption">Find the mates before time runs out.</p>
+                    <Link to="/bullet"><button class = "customPuzzle-button">Bullet</button></Link>
+                    </div>
+                </div>
             </div>
         );
     }
