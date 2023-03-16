7e319f36849d80182b9db1c9ac1675c5a61865a0
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-03-16 13:21:06 -0500

    Navbar and Classical Styling
    Navbar changed from buttons to links, more styling to come. Some styling changes to classical ui page to make it look closer to design documents.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,19 @@ import '../styles/Navbar.css'
 export default class Navbar extends React.Component {
     render() {
         return (
-            <div className="navbar">
-                <Link to="/"></Link>
-                <Link to="/gamemode"><button type="button" className="button">Play</button></Link>
-                <Link to="/custom-puzzles"><button type="button" className="button">Custom Puzzles</button></Link>
-                <Link to="/leaderboard"><button type="button" className="button">Leaderboard</button></Link>
-                <Link to="/help-tutorial"><button type="button" className="button">Help</button></Link>
-                <Link to="/settings"><button type="button" className="button">Settings</button></Link>
-                <Link to="/login"><button type="button" className="button">Login</button></Link>
-            </div>
+
+        <div className="navbar--container">
+            <ul>
+                <li class = "navbar--link"><a href= '/'>Home</a></li>
+                <li class = "navbar--link"><a href= '/gamemode'>Play</a></li>
+                <li class = "navbar--link"><a href= '/custom-puzzles'>Custom Puzzles</a></li>
+                <li class = "navbar--link"><a href= '/leaderboard'>Leaderboard</a></li>
+                <li class = "navbar--link"><a href= '/help-tutorial'>Help</a></li>
+                <li class = "navbar--link"><a href= '/settings'>Settings</a></li>
+                <li class = "navbar--link"><a href= '/login'>Login</a></li>
+            </ul>
+        </div>
+            
         )
     }
 }
