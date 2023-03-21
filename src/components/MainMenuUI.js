80b396145421fb18b25e9740542e25d24c0c200a
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-03-21 16:22:38 -0500

    Styling Main Menu
    Changes made to buttons, consistent color scheme chosen and to be updated across other pages soon.

--- src/components/MainMenuUI.js
+++ src/components/MainMenuUI.js
@@ -23,12 +23,12 @@ export default class MainMenuUI extends React.Component {
                 <br />
                 <div className = "buttons-container">
                 <Link to="/"></Link>
-                <Link to="/gamemode"><button class = "button">Play</button></Link>
-                <Link to="/custom-puzzles"><button class = "button">Custom Puzzles</button></Link>
-                <Link to="/leaderboard"><button class = "button">Leaderboard</button></Link>
-                <Link to="/help-tutorial"><button class = "button">Help</button></Link>
-                <Link to="/settings"><button class = "button">Settings</button></Link>
-                <Link to="/login"><button class = "button">Login</button></Link>
+                <Link to="/gamemode"><button class = "mainmenu--button">Play</button></Link>
+                <Link to="/custom-puzzles"><button class = "mainmenu--button">Custom Puzzles</button></Link>
+                <Link to="/leaderboard"><button class = "mainmenu--button">Leaderboard</button></Link>
+                <Link to="/help-tutorial"><button class = "mainmenu--button">Help</button></Link>
+                <Link to="/settings"><button class = "mainmenu--button">Settings</button></Link>
+                <Link to="/login"><button class = "mainmenu--button">Login</button></Link>
                 </div>
             </div>
         )
