6c0f7b1143b02ea9d8bba18017325524e3c9fad4
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-03-21 18:32:35 -0500

    Update Navbar.js
    Reordering of links in navbar to follow along with main menu ordering.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,11 @@ export default class Navbar extends React.Component {
                 <li class = "navbar--link--home"><a href= '/'>Checkmate</a></li>
                 <li class = "navbar--link"><a href= '/gamemode'>Play</a></li>
                 <li class = "navbar--link"><a href= '/custom-puzzles'>Custom Puzzles</a></li>
-                <li class = "navbar--link"><a href= '/leaderboard'>Leaderboard</a></li>
-                <li class = "navbar--link"><a href= '/help-tutorial'>Help</a></li>
-                <li class = "navbar--link"><a href= '/settings'>Settings</a></li>
                 <li class = "navbar--link"><a href= '/login'>Login</a></li>
+                <li class = "navbar--link"><a href= '/leaderboard'>Leaderboard</a></li>
+                <li class = "navbar--link"><a href= '/settings'>Settings</a></li>
+                <li class = "navbar--link"><a href= '/help-tutorial'>Help</a></li>
             </ul>
-            
         )
     }
 }
