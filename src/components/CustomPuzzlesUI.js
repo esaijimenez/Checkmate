4e5b1e0a997c721563eeba3a56c5fea5e33343c5
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-03-28 12:51:54 -0500

    Styling Custom Puzzle Choice Page
    Styled consistently with the Game Mode selection page.

--- src/components/CustomPuzzlesUI.js
+++ src/components/CustomPuzzlesUI.js
@@ -12,9 +12,10 @@ export default class CustomPuzzlesUI extends React.Component {
         </Link>
 
         <div className="customPuzzle-container">
-          <div className="customPuzzle-item">
+          <div className="customPuzzle-item1">
             <h1 class="customPuzzle-title">Create</h1>
             <p class="customPuzzle-caption">Create your own puzzles.</p>
+            <p class = 'gameMode--page--img'>&#9819;</p>
             <div className="customPuzzle-buttons-container">
               <Link to="/create">
                 <button class="customPuzzle-button">Create</button>
@@ -22,9 +23,10 @@ export default class CustomPuzzlesUI extends React.Component {
             </div>
           </div>
 
-        <div className="customPuzzle-item">
+        <div className="customPuzzle-item2">
           <h1 class="customPuzzle-title">Play</h1>
           <p class="customPuzzle-caption">Play user created puzzles.</p>
+          <p class = 'gameMode--page--img'>&#9818;</p>
           <div className="customPuzzle-buttons-container">
             <Link to="/play">
               <button class="customPuzzle-button">Play</button>
