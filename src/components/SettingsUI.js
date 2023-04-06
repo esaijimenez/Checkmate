b0acb814b6a2a54cab290a51de85cad23c962c3a
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-04-06 11:34:43 -0500

    Setting up Settings Styling

--- src/components/SettingsUI.js
+++ src/components/SettingsUI.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Navbar from './Navbar.js';
+import "../styles/Settings-style.css";
+
 export default class Settings extends React.Component {
     constructor(props) {
         super(props);
@@ -15,9 +18,11 @@ export default class Settings extends React.Component {
 
     render() {
         return (
-            <div>
-                <Link to="/"><button>Back</button></Link>
-                <h1>Settings</h1>
+            <div className = "settings">
+                <Navbar/>
+                <h1 class="settings--title">Settings</h1>
+
+                
             </div>
         );
     }
