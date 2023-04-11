1d3871fcb95123c7f61c2e9377b5f17accd871e1
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-04-11 13:12:08 -0500

    Settings User Scores Table
    Also installed Firebase updates.

--- src/components/SettingsUI.js
+++ src/components/SettingsUI.js
@@ -1,29 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Navbar from './Navbar.js';
+import Navbar from "./Navbar.js";
 import "../styles/Settings-style.css";
 
 export default class Settings extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    };
+  componentDidMount() {}
 
-    componentDidMount() {
+  render() {
+    return (
+      <div className="settings">
+        <Navbar />
+        <h1 class="settings--title">Settings</h1>
 
-    };
+        <div className="settings--container">
+          <div className="user--leaderboard">
+            <h2>Recent Scores</h2>
 
-    render() {
-        return (
-            <div className = "settings">
-                <Navbar/>
-                <h1 class="settings--title">Settings</h1>
+            <table class="user--leaderboard--results">
+              <thead>
+                <tr>
+                  <th class="user--th">Score</th>
+                  <th class="user--th">Time</th>
+                  <th class="user--th">Date</th>
+                </tr>
+              </thead>
+              <tbody class="user--scores--table"></tbody>
+            </table>
+          </div>
 
-                
-            </div>
-        );
-    }
+        <h2>Options</h2>
+        
+
+        </div>
+      </div>
+    );
+  }
 }
