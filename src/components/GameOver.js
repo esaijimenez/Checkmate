609f72d55a34a3a4c18b6a48bb197d989922856b
autumnhale17 <44533926+autumnhale17@users.noreply.github.com>
2023-04-13 10:26:02 -0500

    Update GameOver.js
    Removed "three" lives specification in popup. Technically they get 4 lives (3, 2, 1, 0, then once that one is lost it's game over).

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -7,7 +7,7 @@ const GameOver = () => {
         <div className="popup">
             <div className="popup-content">
                 <h2 class = "popup-message-main">Game Over!</h2>
-                <p class = "popup-message-sub">You have lost all three lives.</p>
+                <p class = "popup-message-sub">You have lost all lives.</p>
                 <div className="popup-buttons">
                     <Link to="/gamemode"><button class = 'popup-button-1'>Restart Game</button></Link>
                     <Link to="/"><button class = 'popup-button-2'>Return Home</button></Link>
