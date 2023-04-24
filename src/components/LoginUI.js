edc4776afd1c5c8219801e03ed5b35658fed9c6f
Esai <ejimenez4@my.apsu.edu>
2023-04-23 23:57:35 -0500

    got a bit closer to figuring out the login, still doesnt work fully

--- src/components/LoginUI.js
+++ src/components/LoginUI.js
@@ -2,32 +2,140 @@ import React, { useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/compat/app';
 import { auth, provider } from '../firebase.js';
 import { signInWithPopup } from 'firebase/auth';
+import { Link } from "react-router-dom";
+import '../styles/LoginUI-style.css'
 import MainMenuUI from './MainMenuUI.js';
+import ReactDOM from 'react-dom';
+import { db } from "../firebase.js";
+import { ref, onValue, set } from 'firebase/database';
 
 export default class LoginUI extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ''
+            email: '',
+            allUID: [],
+            userUID: '',
+            localStorageEmail: '',
+            localStorageUID: '',
+            isLoggedIn: false,
+            isUserInDatabase: true,
+            userCounter: 0,
         };
     };
 
+    componentDidMount() {
+        const userRef = ref(db, '/users');
+        onValue(userRef, (snapshot) => {
+            const count = snapshot.size;
 
-    handleClick = () => {
-        signInWithPopup(auth, provider).then((data) => {
-            this.setState({ email: data.user.email })
-            localStorage.setItem("email", data.user.email)
+            let UID = [];
+            snapshot.forEach((userSnapshot) => {
+                UID.push({
+                    UID: userSnapshot.child("userID").val()
+                })
+            });
+
+            this.setState({
+                userCounter: count,
+                allUID: UID
+            })
+            console.log("User Count: ", this.state.userCounter)
+            console.log("All UIDs: ", this.state.allUID)
         })
     }
 
-    render() {
-        return (
-            <div>
-                {this.state.email ? <MainMenuUI /> :
-                    <button onClick={this.handleClick}>SignIn with Google</button>
-                }
-            </div>
 
+    handleLogin = () => {
+
+        signInWithPopup(auth, provider).then((data) => {
+            this.setState({ email: data.user.email })
+            this.setState({ userUID: data.user.uid })
+            localStorage.setItem("email", data.user.email)
+            localStorage.setItem("userUID", data.user.uid)
+        })
+        this.state.localStorageEmail = localStorage.getItem("email")
+        this.state.localStorageUID = localStorage.getItem("userUID")
+        //console.log(this.state.localStorageEmail)
+        //console.log(this.state.localStorageUID)
+        if (this.state.email !== null) {
+            this.setState({
+                isLoggedIn: true
+            })
+        }
+
+        for (let i = 0; i <= this.state.allUID.length; i++) {
+            console.log("Inside for loop ");
+            console.log("this.state.allUID: ", this.state.allUID[i])
+            console.log("this.state.localStorageUID: ", this.state.localStorageUID)
+
+            if (this.state.allUID[i] != this.state.localStorageUID && this.state.localStorageUID !== null) {
+                console.log("This UserID was not found in database");
+                this.state.isUserInDatabase = false;
+            }
+        }
+
+
+        console.log("this.state.isUserInDatabase: ", this.state.isUserInDatabase);
+
+        if (this.state.isUserInDatabase === false) {
+            console.log("Inside of an If statement that should be IMPOSSIBLE")
+            //console.log("this.state.allUID: ", this.state.allUID)
+            //console.log("this.state.localStorageUID: ", this.state.localStorageUID)
+
+            const mateRef = ref(db, "/users/" + this.state.userCounter);
+            set(mateRef, {
+                username: "",
+                userID: this.state.localStorageUID,
+                userSettings: "",
+            });
+
+            this.setState({
+                userCounter: this.state.userCounter + 1
+            })
+        }
+
+    }
+
+    handleNavToMainMenu = () => {
+        console.log("handleNavToMainMenu")
+        this.props.history.push({
+            pathname: '/'
+        });
+    }
+
+    handleNavToUsername = () => {
+
+    }
+
+    // handleUsernameToDatabase = () => {
+    //     const username = document.getElementById('textbox');
+
+    //     const userRef = ref(db, "/users" + this.state.userCounter);
+    //     set(userRef, {
+    //         username: username,
+    //         userID: this.state.uid
+    //     });
+
+    //     this.setState({
+    //         userCounter: this.state.userCounter + 1
+    //     })
+    // }
+
+    render() {
+        // if (this.state.localStorageUID === null) {
+        //     window.location.reload();
+        // }
+        return (
+            <div className="login-container" >
+                <div className="login-item">
+                    <h1 class="login-title">Login</h1>
+                    <div className="login-buttons-container">
+                        {!this.state.isLoggedIn && <button class="login-button" onClick={this.handleLogin}>Login with Google</button>}
+                        {this.state.email && <button class="login-button" onClick={this.handleNavToMainMenu}>Return Home</button>}
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/components/MainMenuUI.js
+++ src/components/MainMenuUI.js
@@ -11,16 +11,57 @@ export default class MainMenuUI extends React.Component {
         };
     };
 
+    componentDidMount() {
+        //localStorage.clear()
+        //console.log(localStorage.getItem("email"))
+    }
+
+    handleLogout = () => {
+        localStorage.clear()
+        setTimeout(() => {
+            this.props.history.push({
+                pathname: '/login'
+            });
+        }, 100)
+
+        // if (localStorage.getItem("email") !== '') {
+        //     localStorage.clear()
+        //     // window.location.reload()
+        //     console.log(localStorage.getItem("email"))
+        // }
+    }
+
     render() {
+        let email = localStorage.getItem("email")
+        let uid = localStorage.getItem("userUID")
+        let isEmail = true;
+        if (email === null) {
+            isEmail = false;
+        }
+        console.log(email)
+        console.log(uid)
         return (
             <div className="container">
+                {/* {!isEmail && (<div className="popup">
+                    <div className="popup-content">
+                        <h2 class="popup-message-main">Login</h2>
+                        <ul class="popup-message-sub">
+                            <li>Please login first</li>
+                        </ul>
+                        <div className="popup-buttons">
+                            <Link to="/login"><button class='popup-button-1'>OK</button></Link>
+                        </div>
+                    </div>
+                </div>
+                )} */}
+
                 <h1 class="title">Checkmate</h1>
                 <p class="caption">Find the checkmate!</p>
                 <div className="buttons-container">
                     <Link to="/"></Link>
                     <Link to="/gamemode"><button class="mainmenu--button">Play</button></Link>
                     <Link to="/custom-puzzles"><button class="mainmenu--button">Custom Puzzles</button></Link>
-                    <Link to="/login"><button class="mainmenu--button">Login</button></Link>
+                    <button class="mainmenu--button" onClick={this.handleLogout}>Login/Logout</button>
                     <Link to="/leaderboard"><button class="mainmenu--button">Leaderboard</button></Link>
                     <Link to="/settings"><button class="mainmenu--button">Settings</button></Link>
                     <Link to="/help-tutorial"><button class="mainmenu--button">Help</button></Link>
